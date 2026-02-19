da60f0f3ca9668c42c96b925386ea8ef753d4597
DevScissors <sean.scissors@gmail.com>
2026-02-18 20:07:44 -0800

    Update year to be current and fix broken npm packages

--- Projects/reactUdemy_project1/src/components/Expenses/ExpensesFilter.js
+++ Projects/reactUdemy_project1/src/components/Expenses/ExpensesFilter.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import "./ExpensesFilter.css";
+import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
   const filterChangeHandler = (event) => {
@@ -8,14 +8,18 @@ const ExpensesFilter = (props) => {
   };
 
   return (
-    <div className="expenses-filter">
-      <div className="expenses-filter__control">
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selectedYear} onChange={filterChangeHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+          <option value='2026'>2026</option>
+          <option value='2025'>2025</option>
+          <option value='2024'>2024</option>
+          <option value='2023'>2023</option>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
         </select>
       </div>
     </div>
